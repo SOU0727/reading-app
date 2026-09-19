bd290891d09beb3e360da6ae2726fd0b54ac963f
SOU0727 <sou.m0727@gmail.com>
2026-09-19 16:01:28 +0900

    編集機能・星評価・読書統計ページを追加

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDEyFPaUegAoPHpAaB9z1bcbAf7fKQh8JU",
+  authDomain: "reading-app-5527c.firebaseapp.com",
+  projectId: "reading-app-5527c",
+  storageBucket: "reading-app-5527c.firebasestorage.app",
+  messagingSenderId: "778929617611",
+  appId: "1:778929617611:web:9d74f56b2e2244ea2b794e"
+};
+
+const BOOKS_API_KEY = "AIzaSyALQaDbxoBEkh72niF2yUX_VjjExDkP4Lw";
